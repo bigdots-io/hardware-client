f2896aa2bb703a8bc0d65cc64dc906d002fe8eae
Roy Kolak <roy.kolak@gmail.com>
2016-08-20 12:24:13 -0500

    update install script

--- install.sh
+++ install.sh
@@ -6,4 +6,10 @@ cp upstart/hardware-client.conf /etc/init/
 echo "Reloading upstart config"
 initctl reload-configuration
 
+echo "Installing npm dependencies"
+npm install
+
+echo "Starting hardware-client"
+sudo start hardware-client
+
 echo "Done"
