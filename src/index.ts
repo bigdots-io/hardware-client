d960cf9c333634dfeaa245ca621a78d95df50a31
Roy Kolak <roykolak@fastmail.com>
2024-10-29 10:47:59 -0500

    attempt

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ if (!options.emulate) {
   );
 
   matrix.afterSync((mat, dt, t) => {
-    if (options.debug && updateQueue.length > 0) {
+    if (options.debug && updateQueue?.length > 0) {
       console.log("Queue:", updateQueue.length);
     }
 
