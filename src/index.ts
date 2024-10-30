32148731ec31850c5c652d3263fd26c580c41159
Roy Kolak <roykolak@fastmail.com>
2024-10-30 12:00:17 -0500

    work

--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,6 @@ program
   .option("--cols <number>")
   .option("--brightness <number>")
   .option("--chain-length <number>")
-  .option("--multiplexing <number>")
-  .option("--parallel <number>")
   .option("--debug <boolean>")
   .option("--emulate <boolean>");
 
@@ -55,11 +53,6 @@ if (!options.emulate) {
       ...LedMatrix.defaultMatrixOptions(),
       rows: parseInt(options.rows, 10) as MatrixOptions["rows"],
       cols: parseInt(options.cols, 10) as MatrixOptions["cols"],
-      parallel: parseInt(options.parallel, 10) as MatrixOptions["parallel"],
-      multiplexing: parseInt(
-        options.multiplexing,
-        10
-      ) as MatrixOptions["multiplexing"],
       chainLength: parseInt(
         options.chainLength,
         10
